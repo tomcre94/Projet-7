3ba1fe72b88d2af49dc5dc3496b50cbaefe1febc
Tom Creuse <tom.creuse@gmail.com>
2024-04-07 16:07:35 +0200

    crud integrated

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
 const app = express();
 mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -7,23 +8,44 @@ mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/tes
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+  //create and save new book
+  app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const book = new Book({
+      ...req.body
+    });
+    thing.save()
+      .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+  //find a book
+  app.get('/api/stuff/:id', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+      .then(book => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+  });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+  //update a book
+  app.put('/api/stuff/:id', (req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+  // find books
+  app.use('/api/stuff', (req, res, next) => {
+    Book.find()
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  //delete a book
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
 
 module.exports = app;
