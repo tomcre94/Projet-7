e66caa2942622aac4f75d7b9ae0102ef9a59c94a
Tom Creuse <tom.creuse@gmail.com>
2024-04-07 17:29:19 +0200

    book modified

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Book = require('./models/bookModel');
 const app = express();
+const bookRoutes = require('./routes/book');
+
+app.use('/api/book', bookRoutes);
+
 mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-  
-
 
 module.exports = app;
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/bookModel');
 
 //create and save new book
 router.post('/api/stuff', (req, res, next) => {
     delete req.body._id;
     const book = new Book({
-      ...req.body
+        title: req.body.title,
+        author: req.body.author,
+        imageURL: req.body.imageURL,
+        year: req.body.year,
+        genre: req.body.genre,
+        ratings: req.body.ratings
     });
     thing.save()
       .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
       .catch(error => res.status(400).json({ error }));
-  });
+  }); 
 
   //find a book
   router.get('/:id', (req, res, next) => {
@@ -27,7 +33,7 @@ router.post('/api/stuff', (req, res, next) => {
   });
 
   // find books
-  router.use('/stuff', (req, res, next) => {
+  router.get('/'+'', (req, res, next) => {
     Book.find()
       .then(things => res.status(200).json(things))
       .catch(error => res.status(400).json({ error }));
