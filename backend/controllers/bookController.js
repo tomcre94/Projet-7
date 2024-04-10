b5dc5a496cf97cd3e2531208c959763a288c9906
Tom Creuse <tom.creuse@gmail.com>
2024-04-10 12:46:26 +0200

    séparation logique routing

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,6 +1,38 @@
 const Book = require('../models/bookModel');
 
-exports.getAllBooks = async (req, res) => {
+//Create
+//create and save new book 
+exports.createBook = async(req, res) => {
+try { 
+    delete req.body._id;
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        imageURL: req.body.imageURL,
+        year: req.body.year,
+        genre: req.body.genre,
+        ratings: req.body.ratings
+    });
+    await book.save();
+    res.status(201).json({ message: 'Livre enregistré !'})
+} catch(error) {res.status(400).json({ error })};
+}
+
+//Create rating
+
+//Read
+  //find a book
+  exports.getOneBook = async(req, res) => {
+    try {
+        const book = await Book.findOne({ _id: req.params.id });
+        res.json(book);
+    } catch (error) {
+        res.status(404).json({ error });
+    }
+};
+
+  // find all books
+  exports.getAllBooks = async (req, res) => {
     try {
         const books = await Book.find();
         res.status(200).json(books);
@@ -8,3 +40,34 @@ exports.getAllBooks = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+  //best rating
+
+
+//Update
+  //update a book
+exports.updateOneBook = async (req, res) => {
+    try {
+      const result = await Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id });
+      if (result.nModified === 0) {
+        return res.status(404).json({ message: "Livre non trouvé" });
+      }
+      res.status(200).json({ message: "Livre modifié !" });
+    } catch (error) {
+      res.status(500).json({ error: "Erreur lors de la mise à jour du livre" });
+    }
+  };
+  
+//Delete
+  //delete a book
+exports.deleteOneBook = async (req, res) => {
+    try {
+      const result = await Book.deleteOne({ _id: req.params.id });
+      if (result.deletedCount === 0) {
+        return res.status(404).json({ message: "Livre non trouvé" });
+      }
+      res.status(200).json({ message: "Livre supprimé !" });
+    } catch (error) {
+      res.status(500).json({ error: "Erreur lors de la suppression du livre" });
+    }
+  };
+  
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,48 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models/bookModel');
 
-//create and save new book
-router.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-        title: req.body.title,
-        author: req.body.author,
-        imageURL: req.body.imageURL,
-        year: req.body.year,
-        genre: req.body.genre,
-        ratings: req.body.ratings
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  }); 
+const bookCtrl = require('../controllers/bookController');
 
-  //find a book
-  router.get('/:id', (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-      .then(book => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
+router.get('/', bookCtrl.createBook);
+router.get('/', bookCtrl.getOneBook);
+router.get('/', bookCtrl.getAllBooks);
+router.get('/', bookCtrl.updateOneBook);
+router.get('/', bookCtrl.deleteOneBook);
 
-  //update a book
-  router.put('/:id', (req, res, next) => {
-    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Livre modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  // find books
-  router.get('/'+'', (req, res, next) => {
-    Book.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  //delete a book
-  router.delete('/:id', (req, res, next) => {
-    Book.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  module.exports = router;
+module.exports = router;
