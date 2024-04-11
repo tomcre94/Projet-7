65015d20d6b1e80085a579c4490cfdffbbd5fa2f
Tom Creuse <tom.creuse@gmail.com>
2024-04-11 22:50:08 +0200

    user creation is working

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
@@ -14,18 +15,11 @@ mongoose
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-  );
-  next();
-});
+// Middleware cors pour gérer les requêtes Cross-Origin
+app.use(cors());
+
+// Middleware pour parser le corps des requêtes en JSON
+app.use(express.json());
 
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,15 +2,20 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
-        const userId = decodedToken.userId;
-        req.auth = {
-            userId: userId
-        };
-        next();
-    } catch (error) {
-        res.status(401).json({ error });
+  try {
+    if (
+      !req.headers.authorization ||
+      !req.headers.authorization.startsWith('Bearer ')
+    ) {
+      throw new Error('Authorization header missing or invalid');
     }
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+    req.auth = {
+      userId: decodedToken.userId,
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized', error: error.message });
+  }
 };
