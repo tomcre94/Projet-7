622f2794719e4cbb958e2c4505146d18f3af0fc7
Tom Creuse <tom.creuse@gmail.com>
2024-04-11 18:00:04 +0200

    modif server

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,13 @@ const userRoutes = require('./routes/user');
 const app = express();
 const path = require('path');
 
-mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://tomcreuse:s0FxFFXptEFjMDPn@cluster0.py3bqzo.mongodb.net/',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  //mongodb+srv://tomcreuse:sOFxFFXptEFjMDPn@cluster0.py3bqzo.mongodb.net/
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,56 @@
-const express = require('express');
-const app = express();
-const apiRoutes = require('./routes/api');
+const http = require('http');
+const app = require('./app');
+require('dotenv').config();
 
-// Middleware
-app.use(express.json());
-app.use('/api', apiRoutes);
+// renvoi d'un port valide
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+// Ajout du port sur lequel doit tourner express
+console.log(process.env.PORT);
+const port = normalizePort(process.env.PORT || '5000');
+app.set('port', port);
+
+// recherche des différentes erreurs et Gestion de celles-ci
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// Création de mon serveur avec mon app en argument
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
+
+// Port que mon serveur doit écouter
+server.listen(port);
